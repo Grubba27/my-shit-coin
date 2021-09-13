427356c680eccd0bf5cc49f7e97bf9176e485f62
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-09-13 10:10:54 -0300

    added wallet class

--- index.ts
+++ index.ts
@@ -40,10 +40,33 @@ class Chain {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(transaction: Transaction, senderPublicKey: string, signature: string){
+  addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer){
     const newBlock = new Block(this.lastBlock.hash, transaction);
     this.chain.push(newBlock)
   }
 }
 
+class Wallet {
+  public publicKey: string;
+  public privateKey: string;
 
+  constructor() {
+    const keypair = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: {type: 'spki', format: 'pem'},
+      privateKeyEncoding: {type: 'pkcs8', format: 'pem'},
+    });
+    this.publicKey = keypair.privateKey;
+    this.privateKey = keypair.privateKey;
+  }
+
+  sendMoney(amount: number, payeePublicKey: string) {
+    const transaction = new  Transaction(amount, this.publicKey, payeePublicKey);
+
+    const sign = crypto.createSign('SHA256');
+    sign.update(transaction.toString()).end();
+
+    const signature = sign.sign(this.privateKey);
+    Chain.instance.addBlock(transaction, this.publicKey, signature);
+  }
+}
